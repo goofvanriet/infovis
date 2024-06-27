845a4c41481e904235a0d87f8c6790500ae99e94
goof van riet <goof.van.riet@student.uva.nl>
2024-06-27 14:49:24 +0200

    Add deploy undo edits helper function.

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(cd "$(dirname "$0")/.."; pwd -P)
+
+cd $ROOT_DIR
+
+rm -rf _build
+
+python3 scripts/remove_code_input_cells.py
+jupyter-book build .
+ghp-import -n -p -f _build/html
--- scripts/remove_code_input_cells.py
+++ scripts/remove_code_input_cells.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python3
-
 """
 Helper script to ensure every code cell is being hidden before deploying.
 This is done by adding the `remove_input` metadata to each cell.
 """
-
 import nbformat as nbf
 from os import path
-import json
 
 ROOT_DIR = path.realpath(path.join(path.dirname(__file__), '..'))
+
 DATASTORY_NB_FILEPATH = path.join(ROOT_DIR, 'notebooks', 'story.ipynb')
 
 if __name__ == '__main__':
-    try:
-        with open(DATASTORY_NB_FILEPATH, 'r', encoding='utf-8') as f:
-            ntbk = nbf.read(f, as_version=4)
-    except (json.JSONDecodeError, nbf.reader.NotJSONError) as e:
-        print(f"Error reading the notebook file: {e}")
-        exit(1)
+    ntbk = nbf.read(DATASTORY_NB_FILEPATH, nbf.NO_CONVERT)
 
     for cell in ntbk.cells:
         # Retrieve existing cell tags.
@@ -29,7 +22,6 @@ if __name__ == '__main__':
 
         cell['metadata']['tags'] = cell_tags
 
-    with open(DATASTORY_NB_FILEPATH, 'w', encoding='utf-8') as f:
-        nbf.write(ntbk, f)
+    nbf.write(ntbk, DATASTORY_NB_FILEPATH)
 
-    print('NOTE: Some files might have been changed, run "git status" to see which files have changed.')
+print('NOTE: Some files might have been changed, run "git status" to see which files have changed.')
